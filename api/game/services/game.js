5eb004db39f1bf61e25039afa9208c962cb08467
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2022-10-03 15:11:32 -0300

    128. Criando dados com service.create

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -6,6 +6,7 @@
  */
 
 const axios = require('axios')
+const slugify = require('slugify')
 
 async function getGameInfo(slug) {
     const jsdom = require("jsdom")
@@ -28,6 +29,19 @@ module.exports = {
 
         const { data: { products } } = await axios.get(gogApiUrl)
 
-        console.log(await getGameInfo(products[0].slug))
+        console.log(products[0])
+
+        await strapi.services.publisher.create({
+            name: products[0].publisher,
+            slug: slugify(products[0].publisher).toLowerCase(),
+        })
+
+        await strapi.services.developer.create({
+            name: products[0].developer,
+            slug: slugify(products[0].developer).toLowerCase(),
+        })
+
+
+        // console.log(await getGameInfo(products[0].slug))
     }
 };
