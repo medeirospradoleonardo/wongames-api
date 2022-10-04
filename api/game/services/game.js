476d0a6af8766cd20000e33fcb1ffee563e48596
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2022-10-03 23:37:13 -0300

    132. Fazendo upload de imagens com Form Data

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -8,6 +8,10 @@
 const axios = require('axios')
 const slugify = require('slugify');
 
+function timeout(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 async function getGameInfo(slug) {
     const jsdom = require("jsdom")
     const { JSDOM } = jsdom
@@ -71,10 +75,10 @@ async function createManyToManyData(products) {
 async function createGames(products) {
     await Promise.all(
         products.map(async (product) => {
-            const item = await getByName(product.title, "game");
+            const item = await getByName(product.title, "game")
 
             if (!item) {
-                console.info(`Creating: ${product.title}...`);
+                console.info(`Creating: ${product.title}...`)
 
                 const game = await strapi.services.game.create({
                     name: product.title,
@@ -96,10 +100,44 @@ async function createGames(products) {
                     ...(await getGameInfo(product.slug)),
                 });
 
-                return game;
+                await setImage({ image: product.image, game });
+                await Promise.all(
+                    product.gallery
+                        .slice(0, 5)
+                        .map((url) => setImage({ image: url, game, field: "gallery" }))
+                );
+
+                await timeout(2000)
+
+                return game
             }
         })
     );
+}
+
+async function setImage({ image, game, field = "cover" }) {
+    const url = `https:${image}_bg_crop_1680x655.jpg`;
+    const { data } = await axios.get(url, { responseType: "arraybuffer" });
+    const buffer = Buffer.from(data, "base64");
+
+    const FormData = require("form-data");
+    const formData = new FormData();
+
+    formData.append("refId", game.id);
+    formData.append("ref", "game");
+    formData.append("field", field);
+    formData.append("files", buffer, { filename: `${game.slug}.jpg` });
+
+    console.info(`Uploading ${field} image: ${game.slug}.jpg`);
+
+    await axios({
+        method: "POST",
+        url: `http://${strapi.config.host}:${strapi.config.port}/upload`,
+        data: formData,
+        headers: {
+            "Content-Type": `multipart/form-data; boundary=${formData._boundary}`,
+        },
+    });
 }
 
 module.exports = {
