1b83f47ee6d13c556d1bf35b3a8c1684e2255535
medeirospradoleonardo <leonardo_medeiros_prado@hotmail.com>
2022-10-03 23:03:11 -0300

    131. Criando a primeira estrutura dos jogos

--- api/game/services/game.js
+++ api/game/services/game.js
@@ -23,15 +23,15 @@ async function getGameInfo(slug) {
     }
 }
 
-async function getByName(name, entityName){
+async function getByName(name, entityName) {
     const item = await strapi.services[entityName].find({ name })
     return item.length ? item[0] : null
 }
 
-async function create(name, entityName){
+async function create(name, entityName) {
     const item = await getByName(name, entityName)
 
-    if(!item){
+    if (!item) {
         return strapi.services[entityName].create({
             name,
             slug: slugify(name, { lower: true }),
@@ -44,29 +44,63 @@ async function createManyToManyData(products) {
     const publishers = {};
     const categories = {};
     const platforms = {};
-  
+
     products.forEach((product) => {
-      const { developer, publisher, genres, supportedOperatingSystems } = product;
-  
-      genres &&
-        genres.forEach((item) => {
-          categories[item] = true;
-        });
-      supportedOperatingSystems &&
-        supportedOperatingSystems.forEach((item) => {
-          platforms[item] = true;
-        });
-      developers[developer] = true;
-      publishers[publisher] = true;
+        const { developer, publisher, genres, supportedOperatingSystems } = product;
+
+        genres &&
+            genres.forEach((item) => {
+                categories[item] = true;
+            });
+        supportedOperatingSystems &&
+            supportedOperatingSystems.forEach((item) => {
+                platforms[item] = true;
+            });
+        developers[developer] = true;
+        publishers[publisher] = true;
     });
-  
+
     return Promise.all([
-      ...Object.keys(developers).map((name) => create(name, "developer")),
-      ...Object.keys(publishers).map((name) => create(name, "publisher")),
-      ...Object.keys(categories).map((name) => create(name, "category")),
-      ...Object.keys(platforms).map((name) => create(name, "platform")),
+        ...Object.keys(developers).map((name) => create(name, "developer")),
+        ...Object.keys(publishers).map((name) => create(name, "publisher")),
+        ...Object.keys(categories).map((name) => create(name, "category")),
+        ...Object.keys(platforms).map((name) => create(name, "platform")),
     ]);
-  }
+}
+
+async function createGames(products) {
+    await Promise.all(
+        products.map(async (product) => {
+            const item = await getByName(product.title, "game");
+
+            if (!item) {
+                console.info(`Creating: ${product.title}...`);
+
+                const game = await strapi.services.game.create({
+                    name: product.title,
+                    slug: product.slug.replace(/_/g, "-"),
+                    price: product.price.amount,
+                    release_date: new Date(
+                        Number(product.globalReleaseDate) * 1000
+                    ).toISOString(),
+                    categories: await Promise.all(
+                        product.genres.map((name) => getByName(name, "category"))
+                    ),
+                    platforms: await Promise.all(
+                        product.supportedOperatingSystems.map((name) =>
+                            getByName(name, "platform")
+                        )
+                    ),
+                    developers: [await getByName(product.developer, "developer")],
+                    publisher: await getByName(product.publisher, "publisher"),
+                    ...(await getGameInfo(product.slug)),
+                });
+
+                return game;
+            }
+        })
+    );
+}
 
 module.exports = {
     populate: async (params) => {
@@ -75,6 +109,7 @@ module.exports = {
         const { data: { products } } = await axios.get(gogApiUrl)
 
         await createManyToManyData([products[2], products[3]])
+        await createGames([products[2], products[3]])
 
         // console.log(await getGameInfo(products[0].slug))
     }
